1b2951aac6b1b56e9205c7921a155656557a8fa3
Giovanni Aguirre <gaguirre@2600hz.com>
2024-08-08 18:01:40 -0600

    basic two way communication between iframe and parent

--- app_one/src/App.tsx
+++ app_one/src/App.tsx
@@ -1,25 +1,55 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const port1Ref = useRef<MessagePort>();
+
+  useEffect(() => {
+    const channel = new MessageChannel();
+    let iframeRefValue: HTMLIFrameElement;
+
+    const loadHandler = () => {
+      if (iframeRef.current?.contentWindow) {
+        iframeRef.current.contentWindow.postMessage("port2 from parent", "*", [
+          channel.port2,
+        ]);
+        port1Ref.current = channel.port1;
+      }
+    };
+
+    if (iframeRef.current) {
+      iframeRef.current.addEventListener("load", loadHandler);
+    }
+    channel.port1.onmessage = (e) => console.log("Message from Child:", e.data);
+
+    if (iframeRef.current) {
+      iframeRefValue = iframeRef.current;
+    }
+
+    return () => {
+      if (iframeRefValue) {
+        iframeRefValue.removeEventListener("load", loadHandler);
+      }
+    };
+  }, []);
+
+  const sendMessageToChild = () => {
+    if (!port1Ref?.current) return;
+    port1Ref.current.postMessage("hey there, i am the parent");
+  };
 
   return (
-    <>
-      <h1>App One</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>App one</h1>
+      <button onClick={sendMessageToChild}>Send message</button>
+      <iframe
+        ref={iframeRef}
+        src="http://localhost:5174/"
+        title="app two iframe"
+      ></iframe>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- app_two/src/App.tsx
+++ app_two/src/App.tsx
@@ -1,25 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const port2Ref = useRef<MessagePort | null>(null);
+
+  useEffect(() => {
+    const handleIframeMessage = (event: MessageEvent) => {
+      console.log("message received", event);
+      const port2 = event.ports[0];
+      port2Ref.current = port2;
+
+      port2.onmessage = (e: MessageEvent) => {
+        console.log("Message from Parent:", e.data);
+        port2.start();
+      };
+    };
+    window.addEventListener("message", handleIframeMessage);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("message", handleIframeMessage);
+      port2Ref.current?.close();
+    };
+  }, []);
+
+  const handleClick = () => {
+    if (!port2Ref?.current) return;
+    port2Ref.current.postMessage("MESSAGE FROM CHILD!");
+  };
 
   return (
     <>
       <h1>App Two</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>Send message to parent</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
