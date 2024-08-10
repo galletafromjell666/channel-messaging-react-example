1067ebda57ae739562c8fbc0917b9d827694c285
Giovanni Aguirre <gaguirre@2600hz.com>
2024-08-09 23:54:12 -0600

    Add messaging

--- app_one/src/App.tsx
+++ app_one/src/App.tsx
@@ -1,9 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+
+interface Message {
+  id: string; //UUID
+  appName: "parent" | "iframe";
+  text: string;
+  date: Date;
+}
+
+const iframeOrigin = "http://localhost:5174/";
 
 function App() {
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const port1Ref = useRef<MessagePort>();
+
+  const [messageHistory, setMessageHistory] = useState<Message[]>([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const channel = new MessageChannel();
@@ -11,9 +23,12 @@ function App() {
 
     const loadHandler = () => {
       if (iframeRef.current?.contentWindow) {
-        iframeRef.current.contentWindow.postMessage("port2 from parent", "*", [
-          channel.port2,
-        ]);
+        iframeRef.current.contentWindow.postMessage(
+          "port2 from parent",
+          // It is good security practice to send messages only to known origins.
+          iframeOrigin, 
+          [channel.port2]
+        );
         port1Ref.current = channel.port1;
       }
     };
@@ -21,7 +36,11 @@ function App() {
     if (iframeRef.current) {
       iframeRef.current.addEventListener("load", loadHandler);
     }
-    channel.port1.onmessage = (e) => console.log("Message from Child:", e.data);
+    channel.port1.onmessage = (e: MessageEvent) => {
+      setMessageHistory((prevMessageHistory) =>
+        prevMessageHistory.concat([e.data])
+      );
+    };
 
     if (iframeRef.current) {
       iframeRefValue = iframeRef.current;
@@ -34,20 +53,58 @@ function App() {
     };
   }, []);
 
-  const sendMessageToChild = () => {
+  const sendMessageToIframe = (message: Message) => {
     if (!port1Ref?.current) return;
-    port1Ref.current.postMessage("hey there, i am the parent");
+    port1Ref.current.postMessage(message);
+  };
+
+  const handleSendButton = () => {
+    const messageToSend: Message = {
+      id: crypto.randomUUID(),
+      text: message,
+      date: new Date(),
+      appName: "parent",
+    };
+    sendMessageToIframe(messageToSend);
+    setMessage("");
+    setMessageHistory((prevMessageHistory) =>
+      prevMessageHistory.concat([messageToSend])
+    );
   };
 
   return (
-    <div className="container">
-      <h1>App one</h1>
-      <button onClick={sendMessageToChild}>Send message</button>
-      <iframe
-        ref={iframeRef}
-        src="http://localhost:5174/"
-        title="app two iframe"
-      ></iframe>
+    <div className="chat-layout">
+      <div className="chat">
+        <div className="chat-history">
+          {messageHistory.map((m) => {
+            const isIncomingMessage = m.appName === "iframe";
+            return (
+              <div
+                className={`message ${isIncomingMessage ? "incoming" : ""}`}
+                key={m.id}
+              >
+                <p>{m.appName}</p>
+                <p>{m.text}</p>
+                <p>{m.date.toDateString()}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="message-form">
+          <input
+            type="text"
+            name="message"
+            id="message"
+            placeholder="type your message"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button onClick={handleSendButton}>send message</button>
+        </div>
+      </div>
+      <iframe ref={iframeRef} src={iframeOrigin} title="app two iframe" />
     </div>
   );
 }
--- app_two/src/App.tsx
+++ app_two/src/App.tsx
@@ -1,17 +1,36 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+
+interface Message {
+  id: string; //UUID
+  appName: "parent" | "iframe";
+  text: string;
+  date: Date;
+}
 
 function App() {
   const port2Ref = useRef<MessagePort | null>(null);
+  const [message, setMessage] = useState("");
+  const [messageHistory, setMessageHistory] = useState<Message[]>([]);
 
   useEffect(() => {
     const handleIframeMessage = (event: MessageEvent) => {
-      console.log("message received", event);
+      /**
+       * It is good security practice to validate received messages.
+       * If the expected sender's origin is known, enhance validation:
+       *
+       *  if (event.origin !== "http://localhost:5173/") return
+       *
+       */
+      if (event.data !== "port2 from parent") return;
       const port2 = event.ports[0];
       port2Ref.current = port2;
 
       port2.onmessage = (e: MessageEvent) => {
-        console.log("Message from Parent:", e.data);
+        setMessageHistory((prevMessageHistory) =>
+          prevMessageHistory.concat([e.data])
+        );
+
         port2.start();
       };
     };
@@ -24,16 +43,56 @@ function App() {
     };
   }, []);
 
-  const handleClick = () => {
+  const sendMessageToIframe = (message: Message) => {
     if (!port2Ref?.current) return;
-    port2Ref.current.postMessage("MESSAGE FROM CHILD!");
+    port2Ref.current.postMessage(message);
+  };
+
+  const handleSendButton = () => {
+    const messageToSend: Message = {
+      id: crypto.randomUUID(),
+      text: message,
+      date: new Date(),
+      appName: "iframe",
+    };
+    sendMessageToIframe(messageToSend);
+    setMessage("");
+    setMessageHistory((prevMessageHistory) =>
+      prevMessageHistory.concat([messageToSend])
+    );
   };
 
   return (
-    <>
-      <h1>App Two</h1>
-      <button onClick={handleClick}>Send message to parent</button>
-    </>
+    <div className="chat">
+      <div className="chat-history">
+        {messageHistory.map((m) => {
+          const isIncomingMessage = m.appName === "parent";
+          return (
+            <div
+              className={`message ${isIncomingMessage ? "incoming" : ""}`}
+              key={m.id}
+            >
+              <p>{m.appName}</p>
+              <p>{m.text}</p>
+              <p>{m.date.toDateString()}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="message-form">
+        <input
+          type="text"
+          name="message"
+          id="message"
+          placeholder="type your message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button onClick={handleSendButton}>send message</button>
+      </div>
+    </div>
   );
 }
 
